ae23aa830cce03435191434a8df01ebb541c5a4b
LuizFelipeVV <lf_alves@id.uff.br>
2025-01-29 00:06:18 -0300

    NOVIDADE
    - Nova função de cadastro de propriedade(incompleto)

--- lib/data_base.dart
+++ lib/data_base.dart
@@ -1,22 +1,24 @@
 import 'dart:io';
+import 'package:projetofinal/logged_user.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-
 class DataBaseHelper {
-  
   Future<Database> initializedDataBase() async {
     sqfliteFfiInit();
     var databaseFactory = databaseFactoryFfi;
     var path = '${Directory.current.path}/sistema_reservas.db';
-    var database = await databaseFactory.openDatabase(path, options: OpenDatabaseOptions(
-      version: 1,
-      onCreate: (db, version) async {
-        await db.execute('''CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    var database = await databaseFactory.openDatabase(path,
+        options: OpenDatabaseOptions(
+          version: 1,
+          onCreate: (db, version) async {
+            await db.execute(
+                '''CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, 
                                               name VARCHAR NOT NULL,
                                               email VARCHAR NOT NULL,
                                               password VARCHAR NOT NULL)''');
 
-        await db.execute('''CREATE TABLE address (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            await db.execute(
+                '''CREATE TABLE address (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                                   cep VARCHAR NOT NULL UNIQUE,
                                                   logradouro VARCHAR NOT NULL,
                                                   bairro VARCHAR NOT NULL,
@@ -24,7 +26,8 @@ class DataBaseHelper {
                                                   uf VARCHAR NOT NULL,
                                                   estado VARCHAR NOT NULL)''');
 
-        await db.execute('''CREATE TABLE property (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            await db.execute(
+                '''CREATE TABLE property (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                                   user_id INTEGER NOT NULL,
                                                 address_id INTEGER NOT NULL,
                                                   title VARCHAR NOT NULL,
@@ -37,12 +40,14 @@ class DataBaseHelper {
                                                   FOREIGN KEY(user_id) REFERENCES user(id),
                                                 FOREIGN KEY(address_id) REFERENCES address(id))''');
 
-        await db.execute('''CREATE TABLE images (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            await db.execute(
+                '''CREATE TABLE images (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                                 property_id INTEGER NOT NULL,
                                                 path VARCHAR NOT NULL,    
                                               FOREIGN KEY(property_id) REFERENCES property(id))''');
-                                              
-        await db.execute('''CREATE TABLE booking (id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            await db.execute(
+                '''CREATE TABLE booking (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                                 user_id INTEGER NOT NULL,
                                                 property_id INTEGER NOT NULL,
                                                   checkin_date VARCHAR NOT NULL,
@@ -53,14 +58,14 @@ class DataBaseHelper {
                                                   rating REAL,
                                                 FOREIGN KEY(user_id) REFERENCES user(id),
                                                 FOREIGN KEY(property_id) REFERENCES property(id))''');
-      },
-    ));
+          },
+        ));
 
     print('Database initialized');
     return database;
   }
 
-  Future<void> insertUser(String name, String email, String password) async {
+  Future<bool> insertUser(String name, String email, String password) async {
     final db = await initializedDataBase();
     await db.insert(
       'user',
@@ -72,10 +77,11 @@ class DataBaseHelper {
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
     print('User inserted');
+    return true;
   }
 
-
-  Future<Map<String, dynamic>?> checkCredentials(String name, String password) async {
+  Future<Map<String, dynamic>?> checkCredentials(
+      String name, String password) async {
     final db = await initializedDataBase();
     final List<Map<String, dynamic>> maps = await db.query(
       'user',
@@ -92,5 +98,33 @@ class DataBaseHelper {
     }
   }
 
-
+  Future<void> insertProperty(
+      String title,
+      String description,
+      int number,
+      String? complement,
+      double price,
+      int maxGuest,
+      String thumbnail,
+      int cep) async {
+    final db = await initializedDataBase();
+    int? userId = LoggedUser().id;
+    //Consumir API do cep para obter o endereço completo
+    await db.insert(
+      'property',
+      {
+        'user_id': userId,
+        'address_id': 'addressId',
+        'title': title,
+        'description': description,
+        'number': number,
+        'complement': complement,
+        'price': price,
+        'max_guest': maxGuest,
+        'thumbnail': thumbnail,
+      },
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    print('Property inserted');
+  }
 }
