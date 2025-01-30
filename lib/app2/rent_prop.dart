f849328f3758b8cd81257a6d124ed6079273b5d8
João Gabriel Ramos <jgbvramos@id.uff.br>
2025-01-30 20:24:40 -0300

    feat: criando menu com opção de Deslogar e Minhas Reservas

--- lib/app2/rent_prop.dart
+++ lib/app2/rent_prop.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import '../logged_user.dart';
 
 class RentProperty extends StatefulWidget {
   @override
   _RentPropertyState createState() => _RentPropertyState();
 }
-
 
 class _RentPropertyState extends State<RentProperty> {
   @override
@@ -14,7 +14,31 @@ class _RentPropertyState extends State<RentProperty> {
         title: Text('Rent Property'),
       ),
       body: Center(
-        child: Text('Rent Property Screen'),
+        child: Column(
+            children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: PopupMenuButton<String>(
+                itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                  const PopupMenuItem<String>(
+                    value: 'logout',
+                    child: Text('Deslogar'),
+                  ),
+                  const PopupMenuItem<String>(
+                    value: 'reservas',
+                    child: Text('Minhas reservas'),
+                  ),
+                ],
+                onSelected: (String result) {
+                  if (result == 'logout') {
+                    LoggedUser().logout();
+                    Navigator.pushNamedAndRemoveUntil(context, '/', (Route<dynamic> route) => false);
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
