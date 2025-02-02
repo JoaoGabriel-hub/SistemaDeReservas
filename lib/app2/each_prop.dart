7efca32b3e2544bbcacfbc46824449a47447aca9
João Gabriel Ramos <jgbvramos@id.uff.br>
2025-02-02 14:00:38 -0300

    feat: tela para cada propriedade

---
+++ lib/app2/each_prop.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EachProperty extends StatefulWidget {
+  @override
+  _EachPropertyState createState() => _EachPropertyState();
+}
+
+class _EachPropertyState extends State<EachProperty> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Each Property'),
+      ),
+    );
+  }
+}
