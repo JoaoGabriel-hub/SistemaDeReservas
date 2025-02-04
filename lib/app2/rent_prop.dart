12b7b02b3acd991e51af58dfe9f3c6e654c42645
João Gabriel Ramos <jgbvramos@id.uff.br>
2025-02-04 17:23:30 -0300

    feat: ordenando as propriedades por ordem decrescente de rating

--- lib/app2/rent_prop.dart
+++ lib/app2/rent_prop.dart
@@ -31,26 +31,29 @@ class _RentPropertyState extends State<RentProperty> {
   }
 
   Future<void> _loadProperties() async {
-  var db = DataBaseHelper();
-  try {
-    List<dynamic> properties = await db.getAllProperties();
+    var db = DataBaseHelper();
+    try {
+      List<dynamic> properties = await db.getAllProperties();
 
-    // Criar uma cópia mutável da lista
-    List<Map<String, dynamic>> mutableProperties = properties.map((p) => Map<String, dynamic>.from(p)).toList();
+      // Criar uma cópia mutável da lista
+      List<Map<String, dynamic>> mutableProperties = properties.map((p) => Map<String, dynamic>.from(p)).toList();
 
-    // Preencher as avaliações de cada propriedade
-    for (var property in mutableProperties) {
-      property['rating'] = await db.calculateRating(property['id']);
+      // Preencher as avaliações de cada propriedade
+      for (var property in mutableProperties) {
+        property['rating'] = await db.calculateRating(property['id']);
+      }
+
+      // Ordenar por rating em ordem decrescente
+      mutableProperties.sort((a, b) => b['rating'].compareTo(a['rating']));
+
+      setState(() {
+        _properties = mutableProperties;
+        _filteredProperties = mutableProperties;
+      });
+    } catch (e) {
+      print("Erro ao carregar propriedades: \$e");
     }
-
-    setState(() {
-      _properties = mutableProperties;
-      _filteredProperties = mutableProperties;
-    });
-  } catch (e) {
-    print("Erro ao carregar propriedades: $e");
   }
-}
 
 
   Future<void> _filterProperties() async {
